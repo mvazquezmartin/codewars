50a75383db12c6ad19f396d3018aa773a1d1811b
mvazquez <matias.vazquezmartin@gmail.com>
2023-12-13 19:31:46 -0300

    6kyu: bouncing balls

---
+++ 6Kyu/Bouncing Balls/bouncingBalls.js
@@ -0,0 +1,21 @@
+function bouncingBall(h, bounce, window) {
+  if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+    return -1;
+  }
+
+  let count = 0;
+
+  while (h > window) {
+    count++;
+    h *= bounce;
+    if (h > window) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(bouncingBall(3.0, 0.66, 1.5)); // 3
+console.log(bouncingBall(30.0, 0.66, 1.5)); // 15
+console.log(bouncingBall(3.0, 1.0, 1.5)); // -1
