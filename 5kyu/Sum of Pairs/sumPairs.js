9ab312552de31c852200feb52a79005622060386
mvazquez <matias.vazquezmartin@gmail.com>
2023-12-14 15:12:39 -0300

    5kyu: sum of pairs

---
+++ 5kyu/Sum of Pairs/sumPairs.js
@@ -0,0 +1,21 @@
+function sumPairs(ints, s) {
+  const seenNumbers = new Set();
+
+  for (let i = 0; i < ints.length; i++) {
+    const complement = s - ints[i];
+
+    if (seenNumbers.has(complement)) {
+      return [complement, ints[i]];
+    }
+
+    seenNumbers.add(ints[i]);
+  }
+
+  return undefined;
+}
+
+console.log(sumPairs([11, 3, 7, 5], 10));
+
+const mySet = new Set()
+mySet.add(4)
+console.log(mySet)
