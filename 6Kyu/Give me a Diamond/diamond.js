1c0cc8ddddfd131cc1510174c6b45c14b60e32af
mvazquez <matias.vazquezmartin@gmail.com>
2023-12-23 21:56:10 -0300

    8kyu: remove the time

--- 6Kyu/Give me a Diamond/diamond.js
+++ 6Kyu/Give me a Diamond/diamond.js
@@ -3,14 +3,12 @@ function diamond(n) {
 
   let result = "";
 
-  // Upper half of the diamond
   for (let i = 1; i <= n; i += 2) {
     const spaces = " ".repeat((n - i) / 2);
     const stars = "*".repeat(i);
     result += spaces + stars + "\n";
   }
 
-  // Lower half of the diamond
   for (let i = n - 2; i >= 1; i -= 2) {
     const spaces = " ".repeat((n - i) / 2);
     const stars = "*".repeat(i);
@@ -20,5 +18,4 @@ function diamond(n) {
   return result;
 }
 
-console.log(diamond(7));
-
+console.log(diamond(5));
---
+++ 8kyu/Remove the time/shortenToDate.js
@@ -0,0 +1,5 @@
+function shortenToDate(longDate) {
+  return longDate.split(",")[0]
+}
+
+console.log(shortenToDate("Tuesday January 29, 10pm"))
