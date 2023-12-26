9bcfd238cb8e21fa7de07eab989da88ab7cebbb1
mvazquez <matias.vazquezmartin@gmail.com>
2023-12-26 19:07:22 -0300

    6kyu: equal sides of an array

---
+++ 6Kyu/Equal Sides Of An Array/findEvenIndex.js
@@ -0,0 +1,14 @@
+function findEvenIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const leftSum = arr.slice(0, i).reduce((acc, num) => acc + num, 0);
+    const rightSum = arr.slice(i + 1).reduce((acc, num) => acc + num, 0);
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+console.log(findEvenIndex([1,2,3,4,3,2,1]))
